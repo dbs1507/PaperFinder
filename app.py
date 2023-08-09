6f7f9a9a677bbfe3ef3fb75656bdae2ee7a178e1
dbs1507 <danielbloch15@gmail.com>
2023-08-09 09:39:43 -0300

    nova chave api e começo de desenvolvimento das citacoes

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import trendsInterface
 import googleScholar
 import json
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
 
@@ -50,6 +55,39 @@ def buscar():
     print(totalResults)
 
     return render_template('resultado.html', consultas_relacionadas=list(consultas_relacionadas), google_scholar=paginationResponses, dado=dado, totalResults=totalResults)
+
+
+@app.route('/citacoes', methods=['GET'])
+def citacoes():
+    dado = request.args.get('dado')
+    page = request.args.get('page', 1)
+
+    serpapi_scholar_link = request.args.get(
+        'serpapi_scholar_link')  # Obtenha o link do parâmetro da URL
+
+    params = {
+        "engine": "google_scholar",
+        "q": dado,
+        "api_key": os.getenv("API_KEY"),
+        "start": (page - 1) * 10
+    }
+
+    response = requests.get(serpapi_scholar_link, params=params).json()
+
+    link_cited_article = []
+
+    organic_results = response['organic_results']
+    for cited in organic_results:
+        link_cited_article.append(cited['inline_links']
+                                  ['cited_by']['serpapi_scholar_link'])
+
+    cited_article = []
+    for citacoes in link_cited_article:
+        request_cited = requests.get(citacoes, params=params).json()
+        # print(request_cited)
+        cited_article.append(request_cited['organic_results'])
+
+    return render_template('citacoes.html', response_content=cited_article)
 
 
 if __name__ == '__main__':
